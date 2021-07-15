ff790ee09b01b235a935179e35cf9a043cb5496f
Alex Xavier <39957770+alexavier20@users.noreply.github.com>
2021-07-15 15:47:00 -0300

    removed unnecessary files

--- Github-app/Scenes/Home/RepositoriesCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RepositoriesCell.swift
-//  Github-app
-//
-//  Created by Alex Xavier on 31/05/21.
-//  Copyright © 2021 Alex Xavier. All rights reserved.
-//
-
-import Foundation
--- Github-app/Scenes/Home/RepositoriesInteractor.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RepositoriesInteractor.swift
-//  Github-app
-//
-//  Created by Alex Xavier on 31/05/21.
-//  Copyright © 2021 Alex Xavier. All rights reserved.
-//
-
-import Foundation
