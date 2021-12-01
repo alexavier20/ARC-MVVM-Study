ac9fe89ff1592879941b09a30141e084dc10e487
Alex Xavier <alexxavier@Mac-mini-de-Alex.local>
2021-12-01 18:48:53 -0300

    Modificado para arquitetura MVVM-C

---
+++ Github-app/Scenes/Home/HomeViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewModel.swift
+//  Github-app
+//
+//  Created by Alex Xavier on 01/12/21.
+//  Copyright © 2021 Alex Xavier. All rights reserved.
+//
+
+import Foundation
+
+protocol HomeViewModeling {
+    
+}
+
+final class HomeViewModel {
+    init() {
+        
+    }
+}
+
+extension HomeViewModel: HomeViewModeling {
+    
+}
---
+++ Github-app/Scenes/RepositoryDetails/RepositoryDetailsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryDetailsViewModel.swift
+//  Github-app
+//
+//  Created by Alex Xavier on 30/11/21.
+//  Copyright © 2021 Alex Xavier. All rights reserved.
+//
+
+import Foundation
